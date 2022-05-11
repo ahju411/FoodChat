9fd03c28f735aec1ac1724bdf55db439a49a7015
혜성 <hshs321@naver.com>
2022-05-11 15:09:53 +0900

    Revert "리사이클러뷰 조합 중"
    This reverts commit 62101342

--- app/src/main/java/com/example/foodchat/ItemData.java
+++
@@ -1,6 +0,0 @@
-package com.example.foodchat;
-
-import java.io.Serializable;
-
-public class ItemData implements Serializable {
-}
--- app/src/main/java/com/example/foodchat/ViewValue.java
+++
@@ -1,6 +0,0 @@
-package com.example.foodchat;
-
-public enum ViewValue {
-    MANAGEREVIEW,MANAGERESERVATION
-
-}
