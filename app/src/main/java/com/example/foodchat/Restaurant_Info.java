bce502476d2a19099431310b6e63cd358f54aef0
ahju411 <hshs321@naver.com>
2022-05-14 00:10:03 +0900

    카카오 지도 api

--- app/src/main/java/com/example/foodchat/Restaurant_Info.java
+++ app/src/main/java/com/example/foodchat/Restaurant_Info.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Restaurant_Info extends AppCompatActivity {
-    Button go_review_btn,go_reservation_btn;
+    Button go_review_btn,go_reservation_btn,go_map_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +30,15 @@ public class Restaurant_Info extends AppCompatActivity {
             public void onClick(View view) {
                 //예약하기로 이동
                 Intent intent = new Intent(view.getContext(), Restaurant_Reservation.class);
+                startActivity(intent);
+            }
+        });
+
+        go_map_btn = (Button) findViewById(R.id.go_map_btn);
+        go_map_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(),Testmap.class);
                 startActivity(intent);
             }
         });
